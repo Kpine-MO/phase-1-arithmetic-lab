2a43e883e55e7af2ceb236d4a92c4cf0b4882c7f
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-08 19:45:25 +0300

    commit tree ce83c124983fc5aa75ef1d437d4d8a65b87a0fe9

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2
+
+const random = 10;
+
+Math.floor(Math.random() * random) + 1;
+
+const num3 = 10;
+const num4 = 6;
+
+const mod = num3 % num4
+
+const max = 20;
+
+Math.max(max);
